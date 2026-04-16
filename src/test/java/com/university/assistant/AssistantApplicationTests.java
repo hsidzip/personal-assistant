afc0883b7ae7a47e2208a2a8f6aa11b81e5b4518
hsidzip <alisher.karataev@mail.ru>
2026-04-17 01:27:04 +0500

    feat: initial project setup

---
+++ src/test/java/com/university/assistant/AssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
